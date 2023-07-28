ba2233cc5b351d7a266d78d70d486bfde920c4cf
SelfWu <83105465+wushaofeng1997@users.noreply.github.com>
2023-07-28 17:56:32 +0800

    Update index.ts

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -1,6 +1,6 @@
 // const API_BASE_URL = "http://81.71.142.57:10007/";
-const API_BASE_URL = "https://proxy.selfwu.one/";
-// const API_BASE_URL = "https://youtu.czw6667.xyz/";
+// const API_BASE_URL = "https://proxy.selfwu.one/";
+const API_BASE_URL = "https://youtu.czw6667.xyz/";
 
 const TIME_OUT = 5000;
 
