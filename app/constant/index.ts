9a2b77b79377299fb4a9672bb2bef5f9b3eb2f93
SelfWu <83105465+wushaofeng1997@users.noreply.github.com>
2023-07-28 15:06:18 +0800

    Update index.ts

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = "http://81.71.142.57:10007/";
+// const API_BASE_URL = "http://81.71.142.57:10007/";
+const API_BASE_URL = "https://proxy.selfwu.one/7207";
+
 const TIME_OUT = 5000;
 
 export { API_BASE_URL, TIME_OUT };
